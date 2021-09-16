6c2207bffda6c5aa97897bf3d709b43b032d9bc9
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-09-16 20:36:46 +1200

    Add ZSH and start work on update command

--- spotdl.py
+++ spotdl.py
@@ -1,5 +1,6 @@
 import discord
 from discord_slash import SlashCommand
+from discord_slash.utils.manage_commands import create_option, create_choice
 
 from credentials import discord_token, guild_ids
 
@@ -17,10 +18,66 @@ async def on_ready():
 async def _ping(ctx): 
     await ctx.send(f"Pong! ({client.latency*1000:.1f}ms)")
 
-@slash.slash(name="",
-             description="",
+@slash.slash(name="zsh",
+             description="Instructions for users with Zsh terminals",
              guild_ids=guild_ids)
-async def _test(ctx):
-    await ctx.send()
+async def _zsh(ctx):
+    await ctx.send("aa")
+
+@slash.slash(name="update",
+             description="Various update instructions",
+             guild_ids=guild_ids,
+             options=[
+                create_option(
+                    name="location",
+                    description="Where should the update come from?",
+                    option_type=3,
+                    required=True,
+                    choices=[
+                        create_choice(
+                            name="pip",
+                            value="pip"
+                                ),
+                        create_choice(
+                            name="master",
+                            value="master"
+                                ),
+                        create_choice(
+                            name="dev",
+                            value="dev"
+                                )
+                        ]
+                        ),
+               create_option(
+                    name="clean",
+                    description="Use pip-autoremove?",
+                    option_type=5,
+                    required=False
+                )
+             ]
+        )
+async def update(ctx, location: str, clean: bool = False):
+    if location == "pip":
+        msg = "**Update spotDL:**\n`pip install -U spotdl`"
+    elif location in ["dev", "master"]:
+        msg = f"**Update spotDL from {location}**\n**1.** `pip uninstall spotdl`\n**2.** `pip install https://codeload.github.com/spotDL/spotify-downloader/zip/{location}`"
+
+    if clean == True:
+        msg = "**Full"
+    await ctx.send(content=msg)
+# async def update(ctx, from: str):
+#     await ctx.send("a")
+
+# @slash.slash(name="Various install instructions",
+#              description="",
+#              guild_ids=guild_ids)
+# async def _a(ctx):
+#     await ctx.send("")
+
+# @slash.slash(name="",
+#              description="",
+#              guild_ids=guild_ids)
+# async def _a(ctx):
+#     await ctx.send("")
 
 client.run(discord_token)

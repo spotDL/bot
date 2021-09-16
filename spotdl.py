b1c103807372a6bafb2169711120434802fcdd02
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2021-09-16 19:24:59 +1200

    Initial Commit

---
+++ spotdl.py
@@ -0,0 +1,27 @@
+import discord
+from discord_slash import SlashCommand # Importing the newly installed library.
+
+discord_token = ""
+guild_ids = [] # Put your server ID in this array.
+
+
+client = discord.Client(intents=discord.Intents.all())
+slash = SlashCommand(client, sync_commands=True) # Declares slash commands through the client.
+
+@client.event
+async def on_ready():
+    print("Ready!")
+
+@slash.slash(name="ping",
+             description="Play a round of ping-pong",
+             guild_ids=guild_ids)
+async def _ping(ctx): 
+    await ctx.send(f"Pong! ({client.latency*1000:.1f}ms)")
+
+@slash.slash(name="",
+             description="",
+             guild_ids=guild_ids)
+async def _test(ctx):
+    await ctx.send()
+
+client.run(discord_token)

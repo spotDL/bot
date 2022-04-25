a0bac8c07b4c493683e2eb9f96142e8876a7e0fe
Silverarmor <23619946+Silverarmor@users.noreply.github.com>
2022-04-25 14:56:25 +1200

    Implement logging

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 from credentials import discord_token
 import os
 import interactions
-
 import logging
-logging.basicConfig(level=logging.WARNING)
+import datetime
+
+logfile_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S")#Setting the filename from current date and time
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format="%(levelname)s %(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filemode="a",
+    filename=f".\\logs\\{logfile_name}.log",
+    )
 
 
 client = interactions.Client(
